49d631f02f910eea4d6dbd9d38dc875cce0023ae
Osakpolor Obaseki <obasekiosa@gmail.com>
2021-12-08 20:23:06 +0100

    Implement getTeam API end point

---
+++ src/main/java/com/github/obasekiosa/ipldashboard/controller/TeamController.java
@@ -0,0 +1,33 @@
+package com.github.obasekiosa.ipldashboard.controller;
+
+import com.github.obasekiosa.ipldashboard.model.Team;
+import com.github.obasekiosa.ipldashboard.repository.MatchRepository;
+import com.github.obasekiosa.ipldashboard.repository.TeamRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TeamController {
+
+    private TeamRepository teamRepository;
+    private MatchRepository matchRepository;
+    
+    public TeamController(TeamRepository teamRepository, MatchRepository matchRepository) {
+        this.teamRepository = teamRepository;
+        this.matchRepository = matchRepository;
+    }
+
+    @GetMapping("/team/{teamName}")
+    public Team getTeam(@PathVariable String teamName) {
+        Team team = this.teamRepository.findByTeamName(teamName);
+
+        team.setMatches(this.matchRepository.findLatestMatchesByTeam(teamName, 4));
+
+        return team;
+    }
+    
+
+    
+}
