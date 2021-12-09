8667bb2609a7cc52fc0ffce25d8dbf500a6bc658
Osakpolor Obaseki <obasekiosa@gmail.com>
2021-12-09 18:28:01 +0100

    Implement Home Page

---
+++ src/frontend/src/pages/HomePage.js
@@ -0,0 +1,43 @@
+import { React, useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { MatchDetailCard } from '../components/MatchDetailCard';
+import { MatchSmallCard } from '../components/MatchSmallCard';
+import { TeamTile } from '../components/TeamTile';
+import { Link } from 'react-router-dom';
+
+import './HomePage.scss';
+
+
+export const HomePage = () => {
+
+    const [teams, setTeams] = useState(null);
+
+    useEffect(() => {
+
+        const fetchTeams = async () => {
+            const response = await fetch(`http://localhost:8080/team`);
+            const data = await response.json();
+            setTeams(data);
+        };
+
+        fetchTeams();
+    }, []);
+
+    if (!teams) return null;
+    else if (!teams.length) return <h1>No Team Found</h1>
+
+    return (
+        <div className="HomePage">
+            <div className="header-section">
+                <h1 className="app-name">IPL DashBoard</h1>
+            </div>
+            <div className="team-grid">
+                {
+                    teams.map(team => <TeamTile key={team.id} teamName={team.teamName} team={team}/>)
+                }
+            </div>
+        </div>
+    );
+}
+
+
